722cad2eaed7cc1b9acba83e7109967ecd4c900c
Artem <artem@artemsMac.local>
2020-10-07 09:50:19 +0200

    Updates

---
+++ precache-manifest.d8742099211a8c63542a93037541f2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfff8bc7bf895c64d92d7f864a1899d",
+    "url": "/pokegame/index.html"
+  },
+  {
+    "revision": "6c612ff96a6c08505be7",
+    "url": "/pokegame/static/css/main.42f32ea4.chunk.css"
+  },
+  {
+    "revision": "657c4a92c853053f2f9a",
+    "url": "/pokegame/static/js/2.ecf2d584.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokegame/static/js/2.ecf2d584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c612ff96a6c08505be7",
+    "url": "/pokegame/static/js/main.ea14db7e.chunk.js"
+  },
+  {
+    "revision": "79c73eaf2f44aabcc106",
+    "url": "/pokegame/static/js/runtime-main.39ecc26a.js"
+  }
+]);
